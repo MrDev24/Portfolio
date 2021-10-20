636f4224973b88c1df6d67927f102c861a184f7f
Jhon Robert lopez <itsmerobert0724@gmail.com>
2021-10-20 16:29:08 +0800

    yes

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+//Modal
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+(function () {
+  'use strict'
+
+ 
+  var forms = document.querySelectorAll('.needs-validation')
+
+  
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+          
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+function Reset(){
+ alert("Please fiil out all fields!"); 
+
+
+}
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+myInput.focus()
+})
+
+var d = document.getElementsByTagName("input");
+
+  function reset(){
+    d = "";
+  }
